5e70b7ea7c2a5b3e6570ff40fb24726ca736c5cb
cjo1788 <cjo1788@users.noreply.github.com>
2022-06-17 13:48:50 +0900

    누락파일 커밋

--- RatingMatchProgram/RatingMatchProgram/Global.h
+++ RatingMatchProgram/RatingMatchProgram/Global.h
@@ -6,14 +6,38 @@
 #include <time.h>
 #include <thread>
 #include <queue>
+#include <map>
 
 using namespace std;
 
 #pragma comment(lib, "winmm.lib")
 
-#define DF_RATING_MAX	3000
-#define DF_RATING_MIN	1000
-#define DF_MATCH_RATING_DEFAULT 10
-#define DF_MATCHING_MIN_TIME	3000
-#define DF_MAX_ROOM_COUNT		100
-#define DF_FREE_MATCH_COUNT		1000
+#pragma region const 사용
+const int iRating_Max = 3000;
+const int iRating_Min = 1000;
+const int iMatch_Rating_Default = 10;
+const int iMatching_Min_Time = 3000;
+const int iMatch_Room_Count = 100;
+const int iFree_Match_Count = 1000;
+#pragma endregion
+
+#pragma region define 사용
+//#define DF_RATING_MAX	3000
+//#define DF_RATING_MIN	1000
+//#define DF_MATCH_RATING_DEFAULT 10
+//#define DF_MATCHING_MIN_TIME	3000
+//#define DF_MAX_ROOM_COUNT		100
+//#define DF_FREE_MATCH_COUNT		1000
+#pragma endregion
+
+#pragma region enum 사용
+//enum {
+//	RATING_MAX = 3000,
+//	RATING_MIN = 1000,
+//	MATCH_RATING_DEFAULT = 10,
+//	MATCHING_MIN_TIME = 3000,
+//	MATCH_ROOM_COUNT = 100,
+//	FREE_MATCH_COUNT = 1000
+//};
+#pragma endregion
+

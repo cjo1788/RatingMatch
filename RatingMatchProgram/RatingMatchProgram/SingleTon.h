833905599e1d750ed036734eb8a8cd96b76163df
cjo1788 <cjo1788@users.noreply.github.com>
2022-07-12 14:12:40 +0900

    싱글톤 뮤텍스 추가

--- RatingMatchProgram/RatingMatchProgram/SingleTon.h
+++ RatingMatchProgram/RatingMatchProgram/SingleTon.h
@@ -1,14 +1,18 @@
 #pragma once
 
+#include <mutex>
+
 template < class T >
 class SingleTon
 {
 private:
 	static T* m_Instance;
+	static std::mutex m_mutex;
 
 public:
 	static T* getInstance()
 	{
+		std::lock_guard<std::mutex> guard(m_mutex);
 		if (!m_Instance)
 			m_Instance = new T;
 
@@ -27,3 +31,6 @@ public:
 
 template < class T >
 T* SingleTon<T>::m_Instance = nullptr;
+
+template < class T >
+std::mutex SingleTon<T>::m_mutex;
